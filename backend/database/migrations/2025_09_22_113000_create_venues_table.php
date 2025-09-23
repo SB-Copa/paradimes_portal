51ed20caf0ab87255d210548ff0ee8b54d834562
Andrei Herrera <herreralorenz@yahoo.com>
2025-09-23 17:55:10 +0800

    Updated migration and renamed backend project folder - venues table - venue - reservation type holder - venue table reservation - promotion table - promotion morph - social media - social media morph - marketing owners - marketing company - event marketing owner - event marketing owner type - event type - events - event ticket type

---
+++ backend/database/migrations/2025_09_22_113000_create_venues_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('venues', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique();
+            $table->string('address')->nullable();
+            $table->unsignedBigInteger('region_id');
+            $table->unsignedBigInteger('province_id');
+            $table->unsignedBigInteger('municipality_id');
+            $table->unsignedBigInteger('barangay_id');
+            $table->string('contact_number');
+            $table->string('email')->nullable();
+            $table->json('websites')->nullable();
+            $table->integer('capacity');
+            $table->integer('user_count');
+            $table->integer('table_count');
+            $table->json('menu_images');
+            $table->json('banner_images');
+            $table->json('carousel_images');
+            $table->text('menu');
+            $table->text('about');
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('venues');
+    }
+};
---
+++ backend/database/migrations/2025_09_23_014203_create_event_marketing_owner_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('event_marketing_owner', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('marketing_owner_id');
+            $table->unsignedBigInteger('event_id');
+            $table->unsignedBigInteger('marketing_company_id');
+            $table->unsignedBigInteger('marketing_owner_type_id');
+
+
+            
+            $table->foreign('marketing_owner_id')->references('id')->on('marketing_owners')->onDelete('cascade');
+            $table->foreign('event_id')->references('id')->on('events');
+            $table->foreign('marketing_company_id')->references('id')->on('marketing_company');
+            $table->foreign('marketing_owner_type_id')->references('id')->on('marketing_owner_type');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('event_marketing_owner');
+    }
+};
