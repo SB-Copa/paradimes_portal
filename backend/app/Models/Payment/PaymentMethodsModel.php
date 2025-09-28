fc442389c12954fb25b6374fbdfc95cfe3bf25a2
Andrei Herrera <andreiherrera@Andreis-MacBook-Pro.local>
2025-09-28 21:11:12 +0800

    done adding models for payments

---
+++ backend/app/Models/Payment/PaymentMethodsModel.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models\Payment;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PaymentMethodsModel extends Model
+{
+    /** @use HasFactory<\Database\Factories\Payment\PaymentMethodsModelFactory> */
+    use HasFactory;
+
+    protected $table = 'payment_methods';
+
+    protected $fillable = [
+        'billing_details',
+        'payment_method_type_id'
+    ];
+
+    protected $casts = [
+        'billing_details' => 'array'
+    ];
+}
