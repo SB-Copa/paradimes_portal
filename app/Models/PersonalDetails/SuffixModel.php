bd81fafdb93626f899e92afe15e068f69d7bcfcc
Andrei Herrera <andreiherrera@Andreis-MacBook-Pro.local>
2025-09-28 21:11:12 +0800

    done adding models for payments

--- app/Models/PersonalDetails/SuffixModel.php
+++ app/Models/PersonalDetails/SuffixModel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\PersonalDetails;
 
 use Illuminate\Database\Eloquent\Model;
 
--- app/Models/Users/NonRegisteredUsers.php
+++ app/Models/Users/NonRegisteredUsers.php
@@ -1,15 +1,30 @@
 <?php
 
-namespace App\Models;
+namespace App\Models\Users;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class NonRegisteredUsers extends Model
 {
     //
+    use HasFactory;
+
     protected $table = 'non_registered_users';
 
     protected $fillable = [
-        '',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'suffix_id',
+        'sex_id',
+        'address',
+        'region_id',
+        'province_id',
+        'municipality_id',
+        'barangay_id',
+        'contact_number',
+        'birthdate',
+        'email',
     ];
 }
