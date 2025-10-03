c64dbdebcb470a3cbe149de20226a9d7b1eef48a
Andrei Herrera <andreiherrera@Andreis-MacBook-Pro.local>
2025-10-03 22:38:57 +0800

    removed auth first

--- routes/api.php
+++ routes/api.php
@@ -8,11 +8,11 @@ use Illuminate\Support\Facades\Route;
 
 
 // Route::post('/add-venue',[App\Http\Controllers\Venue\VenueController::class,'store'])->middleware(['auth:marketing_users_session']);
-Route::post('/admin/add-event',[App\Http\Controllers\Marketing\Events\EventsController::class,'storeEvent'])->middleware(['auth:marketing_users_session']);
-Route::get('/admin/event-type',[App\Http\Controllers\Marketing\Events\EventsController::class,'showEventType'])->middleware(['auth:marketing_users_session']);
-Route::get('/admin/event/{eventID}',[App\Http\Controllers\Marketing\Events\EventsController::class,'showEvents'])->middleware(['auth:marketing_users_session']);
+Route::post('/admin/add-event',[App\Http\Controllers\Marketing\Events\EventsController::class,'storeEvent']);
+Route::get('/admin/event-type',[App\Http\Controllers\Marketing\Events\EventsController::class,'showEventType']);
+Route::get('/admin/event/{eventID}',[App\Http\Controllers\Marketing\Events\EventsController::class,'showEvents']);
 Route::get('/admin/event/{eventID}/venue/{venueID}',[App\Http\Controllers\Marketing\Events\Eventscontroller::class,'showEventVenue']);
 Route::get('/admin/event/{eventID}/venue/{venueID}/tables',[App\Http\Controllers\Marketing\Events\Eventscontroller::class,'showEventVenueTables']);
 
 
-Route::get('/admin/venue/{venueID}/event/{eventID}',[App\Http\Controllers\Marketing\Events\EventsController::class,'showVenueEventsTables'])->middleware(['auth:marketing_users_session']);
+Route::get('/admin/venue/{venueID}/event/{eventID}',[App\Http\Controllers\Marketing\Events\EventsController::class,'showVenueEventsTables']);
