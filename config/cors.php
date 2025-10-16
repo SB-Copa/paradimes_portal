54c74228e11c8861e5810b4f04a087f4b2ca45c2
Andrei Herrera <herreralorenz@yahoo.com>
2025-10-16 16:04:58 +0800

    fixed cors

--- config/cors.php
+++ config/cors.php
@@ -17,7 +17,7 @@ return [
 
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://localhost:3000', 'https://staging.portl.com.ph'],
+    'allowed_origins' => ['https://local.portl.com.ph', 'https://staging.portl.com.ph'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
