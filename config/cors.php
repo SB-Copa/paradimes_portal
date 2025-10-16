cb8e9025cc716f9081ff7508efa1f8f52b028e21
Andrei Herrera <herreralorenz@yahoo.com>
2025-10-16 10:16:33 +0800

    added cors

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'], // ['http://localhost:3000'], // your frontend URL
+    'allowed_origins' => ['http://localhost:3000'], // ['http://localhost:3000'], // your frontend URL
 
     'allowed_origins_patterns' => [],
 
